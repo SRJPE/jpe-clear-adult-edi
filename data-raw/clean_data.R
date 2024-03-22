e6f5f457934d6dc1c91391baf8b07cf538701c3a
liz-fw <lstebbins@flowwest.com>
2024-03-22 15:12:09 -0700

    validates xml with updated metadata for clear upstream passage estimates (including stat method), raw counts, and associated metadata

--- data-raw/clean_data.R
+++ data-raw/clean_data.R
@@ -1,20 +1,13 @@
 library(tidyverse)
 library(googleCloudStorageR)
 
-# TODO personnel
-# TODO title
-# TODO keyword set
-# TODO funding
-# TODO project
-# TODO coverage
-
 # pull in data from google cloud ---------------------------------------------------
 gcs_auth(json_file = Sys.getenv("GCS_AUTH_FILE"))
 gcs_global_bucket(bucket = Sys.getenv("GCS_DEFAULT_BUCKET"))
 
-gcs_get_object(object_name = "standard-format-data/standard_daily_redd.csv",
+gcs_get_object(object_name = "adult-holding-redd-and-carcass-surveys/clear-creek/data/clear_redd.csv",
                bucket = gcs_get_global_bucket(),
-               saveToDisk = here::here("data-raw", "standard_daily_redd.csv"),
+               saveToDisk = here::here("data-raw", "clear_daily_redd.csv"),
                overwrite = TRUE)
 
 gcs_get_object(object_name = "standard-format-data/standard_adult_upstream_passage.csv",
@@ -27,38 +20,95 @@ gcs_get_object(object_name = "standard-format-data/standard_adult_passage_estima
                saveToDisk = here::here("data-raw", "standard_adult_passage_estimate.csv"),
                overwrite = TRUE)
 
-redd_raw <- read.csv(here::here("data-raw", "standard_daily_redd.csv")) |>
-  filter(stream == "clear creek")
+pe-dev-bucket/adult-upstream-passage-monitoring/clear-creek/data/clear_passage.csv
+
+redd_raw <- read.csv(here::here("data-raw", "clear_daily_redd.csv"))
 
 upstream_passage_raw <- read.csv(here::here("data-raw", "standard_adult_passage.csv")) |>
-  filter(stream %in% c("clear creek"))
+  filter(stream == "clear creek")
 
 upstream_passage_estimate_raw <- read.csv(here::here("data-raw", "standard_adult_passage_estimate.csv")) |>
-  filter(stream %in% c("clear creek"))
+  filter(stream == "clear creek")
 
-# clean data --------------------------------------------------------------
-# here is where we clean up the data and make sure it all looks as expected
-# check unique values for each column
-# check that all fields are being read in the right way (usually has to do with dates)
 
-# TODO confirm NAs are chinook
+# redd --------------------------------------------------------------------
+# standardize substrate sizes for redd using the Wentworth Scale, created by W.C Krumbein
+# when the size range fell into two categories, they were rounded down
+
+# standarized size ranges lookup
+substrate_class = data.frame("standardized_size_range" = c("<0.25",
+                                                           "0.25-0.5",
+                                                           "0.5-1",
+                                                           "1-2",
+                                                           "2-4",
+                                                           "4-8",
+                                                           '8-16',
+                                                           ">16"),
+                             "redd_substrate_class" = c("fine sand",
+                                                        "medium sand",
+                                                        "coarse sand", "very coarse sand",
+                                                        "very fine gravel", "fine gravel",
+                                                        "medium gravel",
+                                                        "coarse gravel to boulder"))
+
+unique(redd_raw$redd_substrate_size)
+
+redd_substrate_size_lookup <-
+  data.frame("redd_substrate_size" = unique(redd_raw$redd_substrate_size),
+             "standardized_size_range" = c(NA, "1-2", "2-4", "1-2",
+                                           "2-4", "2-4", "0.5-1", "4-8",
+                                           "2-4", "4-8", "4-8", ">16", "8-16")) |>
+  left_join(substrate_class)
+
+# standardize the reaches
+gcs_get_object(
+  object_name = "jpe-model-data/standard_reach_lookup.csv",
+  bucket = gcs_get_global_bucket(),
+  saveToDisk = here::here("data-raw",  "standard-reach-lookup.csv"),
+  overwrite = TRUE
+)
+
+standard_reach_lookup <- read_csv(here::here("data-raw",  "standard-reach-lookup.csv")) |>
+  filter(stream == "clear creek") |>
+  select(reach, standardized_reach)
+
 redd <- redd_raw |>
-  mutate(species = ifelse(is.na(species), "chinook", species)) |>
-  filter(species != "steelhead",
-         run %in% c("spring", "not recorded")) |>
-  select(-c(depth_m, starting_elevation_ft, num_of_fish_on_redd, latitude,
-            longitude, species, pre_redd_substrate_class, tail_substrate_class,
-            redd_substrate_class, stream, year)) |>
+  filter(species == "Chinook",
+         picket_weir_relation == "above") |>
+  left_join(redd_substrate_size_lookup |>
+              select(redd_substrate_size, redd_substrate_class),
+            by = c("redd_substrate_size")) |>
+  left_join(redd_substrate_size_lookup |>
+              select(redd_substrate_size, tail_substrate_class = redd_substrate_class),
+            by = c("tail_substrate_size" = "redd_substrate_size")) |>
+  left_join(redd_substrate_size_lookup |>
+              select(redd_substrate_size, pre_redd_substrate_class = redd_substrate_class),
+            by = c("pre_redd_substrate_size" = "redd_substrate_size")) |>
+  left_join(standard_reach_lookup, by = c("surveyed_reach" = "reach")) |>
+  # redd count is 1 for each observation, but unique redd_id must be used to get a total and avoid double counting
+  mutate(redd_count = 1) |>
+  select(date, redd_id = JPE_redd_id, reach = standardized_reach, fish_on_redd,
+         age, run, redd_count,
+         redd_measured = measured, redd_width, redd_length, pre_redd_depth, redd_pit_depth,
+         redd_tail_depth, pre_redd_substrate_class, redd_substrate_class,
+         tail_substrate_class, velocity)
   glimpse()
 
 up <- upstream_passage_raw |>
-  select(-c(ladder, hours, comments, confidence_in_sex,
-            fork_length, status, dead, temperature, flow,
-            stream)) |>
+  filter(run %in% c("not recorded", "spring", "unknown")) |>
+  select(-c(ladder, fork_length, temperature, flow, stream, hours)) |>
+  mutate(jack_size = case_when(jack_size == "yes" ~ TRUE,
+                               jack_size == "no" ~ FALSE,
+                               TRUE ~ NA)) |>
   glimpse()
 
 up_estimate <- upstream_passage_estimate_raw |>
-  select(-c(ladder, ucl, lcl, confidence_interval, stream)) |>
+  select(-c(ladder, stream, adipose_clipped, ucl, lcl, confidence_interval)) |>
+  # add stat method from USFWS Adult Spring-run Chinook Salmon Monitoring in Clear Creek, California, 2013-2018 Report
+  # https://drive.google.com/drive/u/0/folders/1vv_QV9NdiIc4tlWPPB3UBs1s8idBOiSB
+  mutate(stat_method = case_when(year %in% c(2013:2016, 2018) ~ "generalized additive model (GAM)",
+                                 year == 2017 ~ "raw data",
+                                 TRUE ~ "not recorded")) |>
   glimpse()
 
 # write files -------------------------------------------------------------
